d443da2f1567742d6b4781e94c0c7f3da6ad214c
MiltonBlack <arziblack2@gmail.com>
2023-03-22 07:40:44 +0100

    created database

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,18 @@
-import express from "express"
+import express, { Request, Response } from "express"
 import { ErrorHandlerController } from "./controllers/error-handler"
 import { PingController } from "./controllers/ping"
 import { register } from "./utils/controller"
+import { connectDB } from "./db/db"
 
 export const app = express()
 
 // apply middlewares
 app.use(express.json())
+
+connectDB();
+
+// API Test
+app.get("/", ((req: Request, res: Response) => res.send("hi")))
 
 // register controllers
 register(app).with(
